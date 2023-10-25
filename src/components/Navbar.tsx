7fe556cab6c281f27b3d1728aa9283d8435761bb
vivek <konavivekramakrishna@gmail.com>
2023-10-25 19:28:07 +0530

    check img not working

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -153,7 +153,7 @@ export function ComplexNavbar() {
     >
       <div className="relative mx-auto flex items-center justify-center  text-blue-gray-900 ">
         <div className="ml-1 mr-1">
-          <img src="/assets/crlogo1.png" width={200} />
+          <img src="/crlogo1.png" width={200} />
         </div>
         <div className="hidden lg:block">
           <NavList />
